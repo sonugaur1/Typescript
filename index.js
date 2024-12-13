dcc95a75386a4227577970b7e92fbfbedb987e5a
sonugaur1 <sg9871090@Gmail.com>
2024-12-13 17:44:24 +0530

    basic Typescript

---
+++ index.js
@@ -0,0 +1,5 @@
+var add = function (a, b) {
+    return a + b;
+};
+var result = add(30, 40);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,5 @@
+const addHandler  = (a:number,b:number) =>{
+return a+b
+}
+const Sum = addHandler(30,40)
+console.log(Sum)
